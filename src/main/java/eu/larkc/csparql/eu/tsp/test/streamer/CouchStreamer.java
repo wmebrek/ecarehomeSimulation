d612782839031e44e725462dcaf1f17469f77dfb
wafaa mebrek <wafaa.mebrek@telecom-sudparis.eu>
2021-02-08 21:11:24 +0100

    modif streamer

--- src/main/java/eu/larkc/csparql/eu/tsp/test/streamer/CouchStreamer.java
+++ src/main/java/eu/larkc/csparql/eu/tsp/test/streamer/CouchStreamer.java
@@ -52,108 +52,39 @@ public class CouchStreamer extends RdfStream implements Runnable  {
 
 	public void run() {
 
-		String fileName = "D:/CSPARQL-ReadyToGoPack/examples_files/ann.txt";
+		Random random = new Random();
+		int sensorIndex;
+		//int subjectIndex;
+		int roomIndex;
+		int observationIndex;
+		boolean isCouch;
 
-		//read file into stream, try-with-resources
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+		while(true){
+			try{
 
-			stream.forEach(line -> {
-				try{
-					//String[] split = line.split("\t");
-					String typeCapteur ;
-					String[] split = line.split("\t");
-					String acrTypeCapteur;
-					String room = split[1].substring(split[1].length()-3, split[1].length());
+				isCouch = random.nextBoolean();
+				observationIndex = random.nextInt(Integer.MAX_VALUE);
+				String observationTime = observationIndex + "time";
 
-					switch (split[1].substring(0,1)) {
-						case "D":
-							typeCapteur = "magnetic_door_sensors";
-							acrTypeCapteur = split[1].substring(0,1);
-							break;
-						case "L":
-							typeCapteur = "light_sensors";
-							acrTypeCapteur = split[1].substring(0,1);
-							switch (split[1].substring(0,2)) {
-								case "LS":
-									typeCapteur = "light_sensors";
-									acrTypeCapteur = split[1].substring(0,2);
-									break;
-								case "LA":
-									typeCapteur = "light_switches";
-									acrTypeCapteur = split[1].substring(0,2);
-									break;
-							}
-							break;
-						case "M":
-							if(split[1].substring(0,2).equals("MA")){
-								typeCapteur = "wide_area_infrared_motion_sensors";
-								acrTypeCapteur = split[1].substring(0,2);
-							}
-							else {
-								typeCapteur = "infrared_motion_sensors";
-								acrTypeCapteur = split[1].substring(0, 1);
-							}
-							break;
-						case "T":
-							typeCapteur = "temperature_sensors";
-							acrTypeCapteur = split[1].substring(0,1);
-							break;
-						default:
-							switch (split[1].substring(0,2)) {
-								case "BA":
-									typeCapteur = "sensor_battery_levels";
-									acrTypeCapteur = split[1].substring(0,2);
-									break;
-								default:
-									typeCapteur = "unknown_sensors";
-									acrTypeCapteur = split[1].substring(0,1);
-									break;
-							}
-					}
+				RdfQuadruple q = new RdfQuadruple("_:" + observationIndex, "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#isObservableAt" , observationTime, System.currentTimeMillis());
+				this.put(q);
+				q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa#isObservedBy" , "couch_sensor", System.currentTimeMillis());
+				this.put(q);
+				q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa#hasSimpleResult" , "\""+isCouch+"\"^^http://www.w3.org/2001/XMLSchema#boolean", System.currentTimeMillis());
+				this.put(q);
 
-					String valeurCapteur = split[2];
-					String valeurCapteurNormalize ;
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSS");
 
-					if (isBooleanHerit(valeurCapteur) && typeCapteur != "unknown_sensors"){
-						valeurCapteurNormalize = "\""+ getBooleanFromBooleanValue(valeurCapteur) + "\"^^http://www.w3.org/2001/XMLSchema#boolean";
+				q = new RdfQuadruple( observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasUpperTimeStampValue" , sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
+				this.put(q);
+				q = new RdfQuadruple( observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasLowerTimeStampValue" , sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
+				this.put(q);
 
-						//String typeCapteur = split[0];
+				Thread.sleep(sleepTime);
 
-						UUID observationIndex = UUID.randomUUID(); //random.nextInt(Integer.MAX_VALUE);
-						String observationTime = observationIndex + "time";
-
-						RdfQuadruple q = new RdfQuadruple("_:" + observationIndex, "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#isObservableAt", observationTime, System.currentTimeMillis());
-						this.put(q);
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa/isObservedBy" , split[1], System.currentTimeMillis());
-						this.put(q);
-
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa/madeBySensor" , typeCapteur, System.currentTimeMillis());
-						this.put(q);
-
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#hasLocation" , room, System.currentTimeMillis());
-						this.put(q);
-
-
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa/hasSimpleResult" , valeurCapteurNormalize, System.currentTimeMillis());
-						this.put(q);
-
-						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSS");
-
-						q = new RdfQuadruple( observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasUpperTimeStampValue", sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
-						this.put(q);
-						q = new RdfQuadruple( observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasLowerTimeStampValue", sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
-						this.put(q);
-
-						logger.info("Couch Data sent => {}",line);
-						Thread.sleep(sleepTime);
-					}
-				} catch(Exception e){
-					e.printStackTrace();
-				}
-
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
+			} catch(Exception e){
+				e.printStackTrace();
+			}
 		}
 
 	}
--- src/main/java/eu/larkc/csparql/eu/tsp/test/streamer/TvStreamer.java
+++ src/main/java/eu/larkc/csparql/eu/tsp/test/streamer/TvStreamer.java
@@ -49,112 +49,41 @@ public class TvStreamer extends RdfStream implements Runnable  {
 	}
 
 	public void run() {
+		Random random = new Random();
+		int sensorIndex;
+		int valeurSensor;
+		int roomIndex;
+		int observationIndex;
+		//int numberOfPerson;
+
+		while (true) {
+			try {
+
+				valeurSensor = random.nextInt(100);
+
+				observationIndex = random.nextInt(Integer.MAX_VALUE);
+				String observationTime = observationIndex + "time";
+
+				RdfQuadruple q = new RdfQuadruple("_:" + observationIndex, "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#isObservableAt", observationTime, System.currentTimeMillis());
+				this.put(q);
+				q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa#isObservedBy", "tv_sensor", System.currentTimeMillis());
+				this.put(q);
+				q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa#hasSimpleResult", "\"" + valeurSensor + "\"^^http://www.w3.org/2001/XMLSchema#integer", System.currentTimeMillis());
+				this.put(q);
+
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSS");
+
+				q = new RdfQuadruple(observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasUpperTimeStampValue", sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
+				this.put(q);
+				q = new RdfQuadruple(observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasLowerTimeStampValue", sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
+				this.put(q);
 
 
-		String fileName = "D:/CSPARQL-ReadyToGoPack/examples_files/ann.txt";
+				Thread.sleep(sleepTime);
 
-		//read file into stream, try-with-resources
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-
-			stream.forEach(line -> {
-				try {
-
-					String typeCapteur ;
-					String[] split = line.split("\t");
-					String acrTypeCapteur;
-					String room = split[1].substring(split[1].length()-3, split[1].length());
-
-					switch (split[1].substring(0,1)) {
-						case "D":
-							typeCapteur = "magnetic_door_sensors";
-							acrTypeCapteur = split[1].substring(0,1);
-							break;
-						case "L":
-							typeCapteur = "light_sensors";
-							acrTypeCapteur = split[1].substring(0,1);
-							switch (split[1].substring(0,2)) {
-								case "LS":
-									typeCapteur = "light_sensors";
-									acrTypeCapteur = split[1].substring(0,2);
-									break;
-								case "LA":
-									typeCapteur = "light_switches";
-									acrTypeCapteur = split[1].substring(0,2);
-									break;
-							}
-							break;
-						case "M":
-							if(split[1].substring(0,2).equals("MA")){
-								typeCapteur = "wide_area_infrared_motion_sensors";
-								acrTypeCapteur = split[1].substring(0,2);
-							}
-							else {
-								typeCapteur = "infrared_motion_sensors";
-								acrTypeCapteur = split[1].substring(0, 1);
-							}
-							break;
-						case "T":
-							typeCapteur = "temperature_sensors";
-							acrTypeCapteur = split[1].substring(0,1);
-							break;
-						default:
-							switch (split[1].substring(0,2)) {
-								case "BA":
-									typeCapteur = "sensor_battery_levels";
-									acrTypeCapteur = split[1].substring(0,2);
-									break;
-								default:
-									typeCapteur = "unknown_sensors";
-									acrTypeCapteur = split[1].substring(0,1);
-									break;
-							}
-					}
-
-					String valeurCapteur = split[2];
-
-					//String valeurCapteur = split[3];
-					String valeurCapteurNormalize;
-
-					if (isInteger(valeurCapteur) && typeCapteur != "unknown_sensors") {
-						valeurCapteurNormalize = getTypeValeur(valeurCapteur);
-
-						//String typeCapteur = split[0];
-
-						UUID observationIndex = UUID.randomUUID(); //random.nextInt(Integer.MAX_VALUE);
-						String observationTime = observationIndex + "time";
-
-						RdfQuadruple q = new RdfQuadruple("_:" + observationIndex, "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#isObservableAt", observationTime, System.currentTimeMillis());
-						this.put(q);
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa/isObservedBy", split[1], System.currentTimeMillis());
-						this.put(q);
-
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa/madeBySensor", typeCapteur, System.currentTimeMillis());
-						this.put(q);
-
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#hasLocation", room, System.currentTimeMillis());
-						this.put(q);
-
-
-						q = new RdfQuadruple("_:" + observationIndex, "http://www.w3.org/ns/sosa/hasSimpleResult", valeurCapteurNormalize, System.currentTimeMillis());
-						this.put(q);
-
-						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+SSS");
-
-						q = new RdfQuadruple(observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasUpperTimeStampValue", sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
-						this.put(q);
-						q = new RdfQuadruple(observationTime, "http://w3id.org/ecareathome/patterns/timeinterval.owl#hasLowerTimeStampValue", sdf.format(new Date()) + "^^http://www.w3.org/2001/XMLSchema#dateTime", System.currentTimeMillis());
-						this.put(q);
-
-						logger.info("TV Data sent => {}",line);
-						Thread.sleep(sleepTime);
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
